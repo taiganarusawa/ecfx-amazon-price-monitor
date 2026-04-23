42a35040f3c41a6a56bb5698478b4ba685c73db2
Taiga Narusawa <tigertigertaigaa@gmail.com>
2026-04-23 15:47:22 -0700

    Adding .gitignore

--- price-drop-monitor/src/test/java/com/taiga/pricemonitor/AppTest.java
+++ price-drop-monitor/src/test/java/com/taiga/pricemonitor/AppTest.java
@@ -1,20 +1,11 @@
 package com.taiga.pricemonitor;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void sampleTest() {
+        assertTrue(true);
     }
 }
