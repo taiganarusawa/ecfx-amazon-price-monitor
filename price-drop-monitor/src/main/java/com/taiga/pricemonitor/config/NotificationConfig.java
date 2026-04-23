36a856bb5ac7c01abe416f6b3a0d2e3c7c4e30a3
Taiga Narusawa <tigertigertaigaa@gmail.com>
2026-04-23 16:18:39 -0700

    Notification and product config files

---
+++ price-drop-monitor/src/main/java/com/taiga/pricemonitor/config/NotificationConfig.java
@@ -0,0 +1,13 @@
+package com.taiga.pricemonitor.config;
+
+public class NotificationConfig {
+    private String method;
+    
+    public String getMethod() { 
+        return method; 
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+}
